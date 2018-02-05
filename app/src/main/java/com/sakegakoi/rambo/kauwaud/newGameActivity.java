1d80701f881da77154f7dc6b53caf06d402d503d
ramkdas12 <ramkdas12@gmail.com>
2018-02-05 22:30:10 +0530

    changing images per interval

--- app/src/main/java/com/sakegakoi/rambo/kauwaud/newGameActivity.java
+++ app/src/main/java/com/sakegakoi/rambo/kauwaud/newGameActivity.java
@@ -1,12 +1,19 @@
 package com.sakegakoi.rambo.kauwaud;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ImageView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Random;
 
 public class newGameActivity extends AppCompatActivity {
 
@@ -14,8 +21,64 @@ public class newGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
-        ImageView image = (ImageView) findViewById(R.id.imageView);
+        final ImageView image = (ImageView) findViewById(R.id.imageView);
         image.setImageResource(R.drawable.eagle);
+        String imageString = loadJSONFromAsset();
+        final long timeInterval = 1000;
+        try{
+            JSONObject object = new JSONObject(imageString);
+            JSONObject getObject = object.getJSONObject("images");
+            final JSONArray getArray = getObject.getJSONArray("imageArray");
+            final Handler handler = new Handler();
+            final Runnable r = new Runnable() {
+                public void run(){
+                    try {
+                        int n = getRandomNumber(getArray);
+                        JSONObject imageObject = getArray.getJSONObject(n - 1);
+                        String imageName = imageObject.get("name").toString();
+                        System.out.println(imageName);
+                        int res = getResources().getIdentifier( imageName, "drawable", getApplicationContext().getPackageName());
+                        image.setImageResource(res);
+                        handler.postDelayed(this, 3000);//set to go off again in 3 seconds.
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            };
+            handler.postDelayed(r,3000);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getRandomNumber(JSONArray imageArray) {
+        Random random = new Random();
+        int n = random.nextInt(imageArray.length() - 1) + 1;
+        return n;
+    }
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("image_info.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
     }
 
 }
